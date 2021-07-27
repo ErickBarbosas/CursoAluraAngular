73bab6b7b21d903955337946cc19a182ffa3cfbf
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-07-27 20:50:19 -0300

    Aula Alura

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,23 @@
+import { Component, Output, OnInit, EventEmitter } from "@angular/core";
+
+
+@Component({
+    selector:'app-nova-transferencia',
+    templateUrl:'./nova-transferencia.component.html',
+    styleUrls:['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent{
+  @Output() aoTransferir = new EventEmitter<any>();
+
+
+  valor: number;
+  destino: number;
+
+    transferir(){
+      console.log('Solicitada Nova Transferencia');
+      const valorEmitir = {valor: this.valor,destino: this.destino};
+     this.aoTransferir.emit(valorEmitir)
+
+    }
+
+}
