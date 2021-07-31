7a0b330c5e3de7176e9d1db9661a7b86e5b4cd7e
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-07-31 20:48:09 -0300

    Integrando os extratos com a API

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,17 +1,25 @@
+import {HttpClient} from '@angular/common/http'
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Transferencia } from '../models/transferencia.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciaService {
 private listatransferencia: any [];
+private url = 'http://localhost:3000/transferencias';
 
-constructor() {
+constructor(private httpCliente: HttpClient) {
   this.listatransferencia =[];
 }
 
 get transferencias(){
 return this.listatransferencia;
+}
+
+todas(): Observable<Transferencia[]>{
+  return this.httpCliente.get<Transferencia[]>(this.url);
 }
 
 private hidratar(transferencia: any){
