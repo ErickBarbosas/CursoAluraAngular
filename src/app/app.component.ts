0d4a37a290827a7b8e1cb0b78f95e7f548db9963
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-08-05 21:20:13 -0300

    Inicio Curso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ export class AppComponent {
   title = 'AluraPic';
   url = 'https://conteudo.imguol.com.br/c/noticias/f4/2021/06/16/scarface-ficou-famoso-devido-a-sua-cicatriz-no-rosto-e-vivia-em-uma-reserva-no-quenia-1623882040609_v2_450x450.jpg'
   alt ='Leao'
+
 }
