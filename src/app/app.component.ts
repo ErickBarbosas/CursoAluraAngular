7f10558e9742d1de312f32c455187c64ac95ca56
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-08-05 21:15:52 -0300

    Inicio do novo Curso de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'alurapic';
+  title = 'AluraPic';
+  url = 'https://conteudo.imguol.com.br/c/noticias/f4/2021/06/16/scarface-ficou-famoso-devido-a-sua-cicatriz-no-rosto-e-vivia-em-uma-reserva-no-quenia-1623882040609_v2_450x450.jpg'
+  alt ='Leao'
 }
