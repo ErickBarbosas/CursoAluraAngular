f408207e193d50f08bd515e0ba14e6d003a7ff4a
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-08-11 22:44:20 -0300

    Utilizando a primeira diretiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AluraPic';
 
+    photos = [
+      {
+      url: 'https://img.r7.com/images/moya-leao-branco-albino-atracao-glen-garriff-conservation-lion-sanctuary-23032021161516582?dimensions=768x883',
+      description: 'branco'
+      },
+      {
+        url: 'https://a-static.mlcdn.com.br/618x463/biblia-leao-colorido-letra-grande-naa-san-maquinaria-editorial-sankto/livrariapontocristao/c8e3c040bcc611eb8dcd4201ac18500e/ac20367b1a21850435acd607374559de.jpg',
+        description: 'normal'
+      },
+      {
+        url: 'https://a-static.mlcdn.com.br/618x463/biblia-leao-colorido-letra-grande-naa-san-maquinaria-editorial-sankto/livrariapontocristao/c8e3c040bcc611eb8dcd4201ac18500e/ac20367b1a21850435acd607374559de.jpg',
+        description: 'normal'
+      }
+    ]
+
 }
