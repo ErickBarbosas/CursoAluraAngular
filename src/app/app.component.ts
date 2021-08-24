bd8295f249b76aa48fba1c78243c57c3ed1432ca
ErickBarbosas <santoserick2015@hotmail.com>
2021-08-23 22:23:55 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AluraPic';
 
-    photos = [
-      {
-      url: 'https://img.r7.com/images/moya-leao-branco-albino-atracao-glen-garriff-conservation-lion-sanctuary-23032021161516582?dimensions=768x883',
-      description: 'branco'
-      },
-      {
-        url: 'https://a-static.mlcdn.com.br/618x463/biblia-leao-colorido-letra-grande-naa-san-maquinaria-editorial-sankto/livrariapontocristao/c8e3c040bcc611eb8dcd4201ac18500e/ac20367b1a21850435acd607374559de.jpg',
-        description: 'normal'
-      },
-      {
-        url: 'https://a-static.mlcdn.com.br/618x463/biblia-leao-colorido-letra-grande-naa-san-maquinaria-editorial-sankto/livrariapontocristao/c8e3c040bcc611eb8dcd4201ac18500e/ac20367b1a21850435acd607374559de.jpg',
-        description: 'normal'
-      }
-    ]
+  photos = [
+    { 
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg', 
+      description: 'Leão'
+    },
+    { 
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg', 
+      description: 'Leoa'
+    },
+    { 
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg', 
+      description: 'Leoa'
+    }         
+  ];  
 
 }
