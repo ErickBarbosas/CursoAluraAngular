323640bd78d79e6f9232003e41b80c2e160e4751
ErickBarbosas <santoserick2015@hotmail.com>
2021-09-02 22:10:16 -0300

    Onchanges

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges,SimpleChange } from '@angular/core';
 import { Photo } from '../../photo/photo';
 
 @Component({
@@ -6,13 +6,27 @@ import { Photo } from '../../photo/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
-  @Input() Photos:Photo[] = [];
+  @Input() photos:Photo[] = [];
+  rows: any[]=[];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(chenges : SimpleChange){
+
+    if(chenges.photos)
+    {
+      this.rows = this.groupColumns(this.photos);
+    }
   }
 
+  groupColumns(photos: Photo[]){
+    const newRows=[];
+
+    for(let index = 0; index < photos.length; index+=3){
+        newRows.push(photos.slice(index,index +3));
+      }
+      return newRows
+  }
 }
